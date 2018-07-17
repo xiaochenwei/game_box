7bf91fa78f5fb09b955e243e8e5e4ae4f3df4795
chenwei <18913171470@126.com>
2018-07-17 17:04:45 +0800

    格式化代码

--- pages/index/index-config.js
+++ pages/index/index-config.js
@@ -1,35 +1,28 @@
-var local_database = [{
-  imgSrc: "../../images/g1.jpg",
-  foodName: "英雄传说：碧之轨迹",
-  id: "wxd56c8dba9aa2f67b",
-  price: 180.00,
-  num: 1
-}, {
-  imgSrc: "../../images/g2.jpg",
-  foodName: "仙剑奇侠传五前传",
-  id: "wx7ca4ba970c1ea3d8",
-  price: 200.00,
-  num: 3
-}, {
-  imgSrc: "../../images/g3.jpg",
-  foodName: "仙剑奇侠传四配音版",
-  id: "wxd56c8dba9aa2f67b",
-  price: 300.00,
-  num: 8
-}, {
-  imgSrc: "../../images/g4.jpg",
-  foodName: "幻想三国志五",
-  id: "wxd56c8dba9aa2f67b",
-  price: 300.00,
-  num: 8
-}, {
-  imgSrc: "../../images/g5.jpg",
-  foodName: "英雄传说：零之轨迹",
-  id: "wxd56c8dba9aa2f67b",
-  price: 300.00,
-  num: 8
-}
-]
+const gameData = {
+  //轮播图
+  banner_list: [
+    { "pic_url": "../../images/banner1.jpg" },
+    { "pic_url": "../../images/banner2.jpg" },
+    { "pic_url": "../../images/banner3.jpg" }
+  ],
+  //新游上线
+  new_list: [
+    { "name": "河洛群侠传", "pic_url": "../../images/h1.jpg" },
+    { "name": "仙剑奇侠传", "pic_url": "../../images/h2.jpg" },
+    { "name": "武侠", "pic_url": "../../images/h3.jpg" },
+    { "name": "破东荒", "pic_url": "../../images/h4.jpg" }
+  ],
+  //游戏列表
+  game_list: [
+    { id: "wxd56c8dba9aa2f67b", src: "../../images/g1.jpg", name: "英雄传说：碧之轨迹", num: 180 },
+    { id: "wx7ca4ba970c1ea3d8", src: "../../images/g2.jpg", name: "仙剑奇侠传五前传", num: 200 },
+    { id: "wxd56c8dba9aa2f67b", src: "../../images/g3.jpg", name: "仙剑奇侠传四配音版", num: 300 },
+    { id: "wxd56c8dba9aa2f67b", src: "../../images/g4.jpg", name: "幻想三国志五", num: 223 },
+    { id: "wxd56c8dba9aa2f67b", src: "../../images/g5.jpg", name: "英雄传说：零之轨迹", num: 480 }
+  ]
+};
 module.exports = {
-  postList: local_database
+  postList: gameData.game_list,
+  bannerList: gameData.banner_list,
+  newList: gameData.new_list,
 }
--- pages/index/index.js
+++ pages/index/index.js
@@ -8,129 +8,83 @@ Page({
     // height: "",
     image_url: IMG_URL,
     showModalStatus: false,
-    cartListShow: true,
+    gameListShow: true,
     postList: postData.postList,
-    motto: 'MiHome_Store',
-    userInfo: {},
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
     duration: 100,
-    "banner_list": [
-      {
-        "banner": [
-          {
-            "pic_url": "../../images/banner1.jpg",
-          },
-          {
-            "pic_url": "../../images/banner2.jpg",
-          },
-          {
-            "pic_url": "../../images/banner3.jpg",
-          }
-        ]
-      }
-    ],
-    hotgoods: [
-      {
-        "name": "河洛群侠传",
-        "pic_url": "../../images/h1.jpg"
-      },
-      {
-        "name": "仙剑奇侠传",
-        "pic_url": "../../images/h2.jpg",
-        "url": "http://home.mi.com/shop/detail?gid=95"
-      },
-      {
-        "name": "武侠",
-        "pic_url": "../../images/h3.jpg"
-      },
-      {
-        "name": "破东荒",
-        "pic_url": "../../images/h4.jpg"
-      }
-      
-    ],
-    gamelist: [{
-      "name": "幻想三国志",
-      "pic_url": "../../images/g1.jpg"
-    }]
+    bannerList: postData.bannerList,
+    newList: postData.newList
   },
   //事件处理函数
-  viewHeight: function (e) {
+  viewHeight: function(e) {
     var winW = wx.getSystemInfoSync().windowWidth;
-    var viewH = (winW <= 320 ? 350 : (winW <= 360 ? 305 : (winW<=375 ? 295 : 265))) + "rpx";
+    var viewH = (winW <= 320 ? 350 : (winW <= 360 ? 305 : (winW <= 375 ? 295 : 265))) + "rpx";
     this.setData({
       height: viewH
     })
   },
 
-  showModal: function (e) {
+  showModal: function(e) {
     var currentStatu = e.currentTarget.dataset.statu;
     this.util(currentStatu);
   },
-  util: function (currentStatu) {
+  util: function(currentStatu) {
     if (currentStatu == "open") {
-      console.log(1111)
-      this.setData(
-        {
-          showModalStatus: true
-        }
-      );
+      this.setData({
+        showModalStatus: true
+      });
     };
-  if(currentStatu == "close") {
-    console.log(1111)
-    this.setData(
-      {
+    if (currentStatu == "close") {
+      this.setData({
         showModalStatus: false
-      }
-    );
-  }
+      });
+    }
   },
-  download: function () {
+  download: function() {
     wx.saveImageToPhotosAlbum({
-      success(res) {
-      }
+      success(res) {}
     })
   },
-  saveImgToPhotosAlbumTap: function () {
+  saveImgToPhotosAlbumTap: function() {
     wx.downloadFile({
       url: IMG_URL,
-      success: function (res) {
+      success: function(res) {
         console.log(res)
         wx.saveImageToPhotosAlbum({
           filePath: res.tempFilePath,
-          success: function (res) {
+          success: function(res) {
             console.log(res)
           },
-          fail: function (res) {
+          fail: function(res) {
             console.log(res)
             console.log('fail')
           }
         })
       },
-      fail: function () {
+      fail: function() {
         console.log('fail')
       }
     })
   },
-  goToTop: function () {
+  goToTop: function() {
     wx.pageScrollTo({
       scrollTop: 0,
       duration: 300
     })
   },
 
-  bindViewTap: function () {
+  bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
+    app.getUserInfo(function(userInfo) {
       //更新数据
       that.setData({
         userInfo: userInfo
